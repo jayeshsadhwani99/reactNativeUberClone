3500f0ffa05476315c18c1f936f5dd34de10a161
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-08-02 20:45:44 +0530

    Google autocomplete lib

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import {store} from './store';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
