fab1ab4cfa5023a1a92adbad0d25b055969e0171
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-08-02 11:59:02 +0530

    Home Screen component

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {Provider} from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import {store} from './store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <SafeAreaView>
-        <Text>Hello World</Text>
-      </SafeAreaView>
+      <HomeScreen />
     </Provider>
   );
 };
