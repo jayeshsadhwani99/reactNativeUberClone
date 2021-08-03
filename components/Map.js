e9585ff512f8409b25c5d1e27f3e5d9373160cf1
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-08-03 16:22:45 +0530

    Map zoom

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {StyleSheet} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import {useSelector} from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
-import {selectOrigin} from '../slices/navSlice';
+import {selectDestination, selectOrigin} from '../slices/navSlice';
+import {GOOGLE_MAPS_APIKEY} from '@env';
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  React.useEffect(() => {
+    if (!origin || !destination) return;
+
+    // Zoom and fit through markers
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: {top: 50, right: 50, left: 50, bottom: 50},
+    });
+  }, [origin, destination]);
 
   return (
     <MapView
+      ref={mapRef}
       provider={PROVIDER_GOOGLE}
       style={tw`flex-1`}
       mapType="mutedStandard"
@@ -19,6 +33,16 @@ const Map = () => {
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}>
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="black"
+        />
+      )}
+
       {origin?.location && (
         <Marker
           coordinate={{
